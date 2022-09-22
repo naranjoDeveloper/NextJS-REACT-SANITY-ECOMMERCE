b05b7aef04720b1a01d431782f2fcae4f3af3c53
naranjo <naranjo.developer@gmail.com~>
2022-09-22 18:22:55 -0500

    start

---
+++ ecommerce/pages/_app.js
@@ -0,0 +1,16 @@
+import { Layout } from '../components'
+import { StateContext } from '../context/StateContext'
+import '../styles/globals.css'
+import {Toaster} from 'react-hot-toast'
+
+function MyApp({ Component, pageProps }) {
+  return(
+  <StateContext >
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </StateContext>
+  )
+}
+
+export default MyApp
